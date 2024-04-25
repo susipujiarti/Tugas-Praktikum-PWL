dad9be35ffd95873bfc66755c6b4ed796aa911ad
pujiherees <pujiartisusi@gmail.com>
2024-04-25 23:14:51 +0700

    Tugas 2 - Migration & Seeders

---
+++ database/migrations/2024_04_25_160127_create_mahasiswa_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->id('id_mahasiswa');
+            $table->string('nim', 9)->unique();
+            $table->string('nama', 50);
+            $table->string('jurusan', '50');
+            $table->text('alamat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+};
